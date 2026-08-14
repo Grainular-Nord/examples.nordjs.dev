8dfd9480ca415f10903ae1e8a99dc218f66d91f5
Sebastian Heinz <sebastian@iamsebastian.dev>
2026-08-15 00:23:40 +0200

    chore: align examples

--- scripts/assemble-cloudflare-assets.mjs
+++ scripts/assemble-cloudflare-assets.mjs
@@ -6,7 +6,7 @@ const examples = ['forms', 'game-of-life', 'hnr', 'minesweeper', 'tic-tac-toe'];
 
 await rm(outputDirectory, { force: true, recursive: true });
 await mkdir(outputDirectory, { recursive: true });
-await cp('pages/index.html', resolve(outputDirectory, 'index.html'));
+await cp('index.html', resolve(outputDirectory, 'index.html'));
 
 for (const example of examples) {
     await cp(resolve('examples', example, 'dist'), resolve(outputDirectory, example), { recursive: true });
